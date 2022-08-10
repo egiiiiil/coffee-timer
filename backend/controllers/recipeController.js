5a645aedc2bff0ca4b0e48e0d976b5619556cdaa
egiiiiil <79319021+egiiiiil@users.noreply.github.com>
2022-08-10 11:32:54 +0200

    Connected to MongoDB, rewrote getRecipes

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,8 +1,47 @@
+import collections from '../config/db.js'
+
+const recipeCollection = collections.recipe
+
 // @desc		Get Recipes
 // @route		GET /api/recipes
 // @access	Private
-const getRecipes = (req, res) => {
-	res.status(200).json({ message: 'Get Recipes' })
+const getRecipes = async (req, res) => {
+	let filter = {}
+	const query = req.query
+	if (query.category) {
+		filter = { type: query.category }
+	}
+	try {
+		const items = await recipeCollection.find({}).toArray()
+		res.status(200).json(items)
+	} catch (err) {
+		console.log(err.message)
+	}
 }
 
-export { getRecipes }
+// @desc		Post Recipes
+// @route		POST /api/recipes
+// @access	Private
+const postRecipe = (req, res) => {
+	if (!req.body.text) {
+		res.status(400)
+		throw new Error('Please add a text field')
+	}
+	res.status(200).json({ message: 'Set Recipes' })
+}
+
+// @desc		Update goal
+// @route 	PUT /api/recipes:id
+// @access	Private
+const updateRecipe = (req, res) => {
+	res.status(200).json({ message: `Update Recipe ${req.params.id}` })
+}
+
+// @desc		Delete goal
+// @route 	DELETE /api/recipes:id
+// @access	Private
+const deleteRecipe = (req, res) => {
+	res.status(200).json({ message: `Delete Recipe ${req.params.id}` })
+}
+
+export { getRecipes, postRecipe, updateRecipe, deleteRecipe }
--- backend/routers/recipeRoute.js
+++ backend/routers/recipeRoute.js
@@ -1,8 +1,14 @@
 import express from 'express'
-import { getRecipes } from '../controllers/recipeController.js'
+import {
+	getRecipes,
+	postRecipe,
+	updateRecipe,
+	deleteRecipe,
+} from '../controllers/recipeController.js'
 
 const recipeRouter = express.Router()
 
-recipeRouter.route('/').get(getRecipes)
+recipeRouter.route('/').get(getRecipes).post(postRecipe)
+recipeRouter.route('/:id').put(updateRecipe).delete(deleteRecipe)
 
 export default recipeRouter
